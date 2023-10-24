8832813222bf3f2877d156024da450f53f9cad6c
u <u@DESKTOP-BEU6QF0>
2023-10-24 14:45:40 +0530

    Masters module

---
+++ Empirehomecrm/src/test/java/com/testcases/CoditionTest.java
@@ -0,0 +1,76 @@
+package com.testcases;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.base.Basetest;
+import com.pageobjects.ConditionFunctionality;
+import com.pageobjects.Loginfunctionality;
+
+public class CoditionTest extends Basetest {
+	
+	Loginfunctionality lf;
+	ConditionFunctionality condition;
+	
+	public CoditionTest() {
+		super();
+	}
+	
+	@BeforeTest
+	public void setup() {
+		
+		initialization();
+		
+		lf = new Loginfunctionality();
+		lf.verifylogin();
+		
+		condition = new ConditionFunctionality();
+	}
+	
+	
+	@Test (priority = 1)
+	public void addnewconditionvalidation() throws Throwable {
+		
+		condition.verifyaddcondition();
+		
+		List<WebElement> records = driver.findElements(By.tagName("tr"));
+		int recordcount = records.size();
+	    Assert.assertEquals(recordcount, 36);
+	}
+	
+	@Test (priority = 2)
+	public void editconditionvalidation() throws Throwable {
+		
+		condition.verifyeditcondition();
+		
+		String urltest = driver.getCurrentUrl();
+		Assert.assertEquals(urltest, "http://empirehome.myprojectsonline.co.in/Master/Condition");
+		
+		
+	}
+	
+	
+	@Test (priority = 3)
+	public void deleteconditionvalidation() throws Throwable {
+		
+		condition.verifydeletecondition();
+		
+		List<WebElement> records = driver.findElements(By.tagName("tr"));
+		int recordcount = records.size();
+	    Assert.assertEquals(recordcount, 35);
+		
+		
+	}
+	
+	@AfterTest
+	public void teardown() {
+		
+	}
+
+}
---
+++ Empirehomecrm/src/test/java/com/testcases/ShiftTest.java
@@ -0,0 +1,73 @@
+package com.testcases;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.base.Basetest;
+import com.pageobjects.Loginfunctionality;
+import com.pageobjects.ShiftsFunctionality;
+
+public class ShiftTest extends Basetest {
+	
+	Loginfunctionality lf;
+	ShiftsFunctionality shifts;
+	
+	public ShiftTest() {
+		super();
+	}
+	
+	@BeforeTest
+	public void setup() {
+		
+		initialization();
+		
+		lf = new Loginfunctionality();
+		lf.verifylogin();
+		
+		shifts = new ShiftsFunctionality();
+		
+		}
+	
+	@Test (priority = 1)
+	public void addnewshiftvalidation() throws Throwable {
+		
+		shifts.verifyaddnewshift();
+		
+		List<WebElement> records = driver.findElements(By.tagName("tr"));
+		int recordcount = records.size();
+	    Assert.assertEquals(recordcount, 20);
+	}
+
+	
+	@Test (priority = 2)
+	public void editshiftvalidation() throws Throwable {
+		
+		shifts.verifyeditshift();
+		
+		String urltest = driver.getCurrentUrl();
+		Assert.assertEquals(urltest, "http://empirehome.myprojectsonline.co.in/Master/Shifts");
+			
+	}
+	
+	@Test (priority = 3)
+	public void deleteshiftvalidation() throws Throwable {
+		
+		shifts.verifydeleteshift();
+		
+		List<WebElement> records = driver.findElements(By.tagName("tr"));
+		int recordcount = records.size();
+	    Assert.assertEquals(recordcount, 19);
+		
+		}
+	
+	@AfterTest
+	public void teardown() {
+		
+	}
+}
